ea70b968446f30ed3725cfdb29290ba26de21673
Aurelien Rodot <Rodot@users.noreply.github.com>
2025-11-09 12:00:02 +0100

    Setup daisyui and theme

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,13 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+  return [{ title: "Buddy" }, { name: "description", content: "Buddy app" }];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <p>Hello, buddy.</p>
+    </div>
+  );
 }
